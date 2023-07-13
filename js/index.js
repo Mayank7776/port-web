950a27364033b9f40ce46a024954a88e63fe3400
Mayank7776 <111176265+Mayank7776@users.noreply.github.com>
2023-07-13 20:13:10 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+// main
+var typed = new Typed('#element', {
+  strings: ['Graphic Designer.','Web Designer.', 'pasionate coder.'],
+  typeSpeed: 50,
+});
+
+  // // welcome note with captalized firstcase
+  // var name = prompt("what is your name");
+  // var firstchar = name.slice(0,1);
+  
+  // var upperfirstchar = firstchar.toUpperCase();
+  
+  // var restofname = name.slice(1,name.length);
+  
+  // var capitalisedname = upperfirstchar + restofname;
+  
+  // alert("Welcome " + capitalisedname);
+  
+  // // css edit by java script
+  //     let buton1 = document.getElementByclass("btn btn-warning");
+  //     console.log(buton1);
+  
+  //     buton1.addEventListener("mouseover",fun1);
+  //     function fun1(){
+  //         buton1.style.backgroundColor = "red";
+  //     }
+  
